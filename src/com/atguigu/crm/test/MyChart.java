cd54f429fc15fd1f8f60b221f37599cdede377c8
z529627331 <529627331@qq.com>
2016-09-18 19:10:18 +0800

    123
    123

--- src/com/atguigu/crm/test/MyChart.java
+++ src/com/atguigu/crm/test/MyChart.java
@@ -5,6 +5,6 @@ public class MyChart {
 		System.out.println("Hello");
 		System.out.println("local");
 		System.out.println("Server");
-		System.out.println("Server1");
+		System.out.println("Server123");
 	}
 }
